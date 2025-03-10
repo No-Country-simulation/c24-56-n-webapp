15ed6486a567619103f556e7ec1fcded15d5b8b5
Ariel Plaza <arielplazasalinas@gmail.com>
2025-03-10 00:17:03 -0300

    update

--- servidesk/tickets/urls.py
+++ servidesk/tickets/urls.py
@@ -1,9 +1,10 @@
-from . import views
+from .views import TicketListCreate
 from django.urls import path
 
 app_name = 'tickets'
 
 urlpatterns = [
     # path('', views.index, name='tickets_index'),
-    path("crear/", views.crear_ticket, name="tickets_crear"),
+    # path("crear/", views.crear_ticket, name="tickets_crear"),
+    path('crear/', TicketListCreate.as_view(), name='ticket-list-create'),
 ]
