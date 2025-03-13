9e92f049ca8630dd6acea309153be739b78bcaf7
Allan-Morales-Prado <allanmoralesprado@gmail.com>
2025-03-12 22:21:06 -0300

    Combinación users y tickets

--- servidesk/core/urls.py
+++ servidesk/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path("users/", include("users.urls")),
+    path('api/users/', include('users.urls')),
     path('tickets/', include('tickets.urls')),
     path('admin/', admin.site.urls),
 ]
